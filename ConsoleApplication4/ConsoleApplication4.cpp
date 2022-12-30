a837cfaf3cd4a1589c91f04c75252918f1dda19a
Sageev George <sageev@gmail.com>
2022-12-29 16:49:41 -0800

    Removed the old main function

--- ConsoleApplication4/ConsoleApplication4.cpp
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -94,12 +94,6 @@ main(void) {
 	return 0;
 }
 
-/*
-int main()
-{
-    std::cout << "Hello World!\n";
-}
-*/
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
